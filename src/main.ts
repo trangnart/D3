317f07858701a56d47f138c17d29d0965a71b574
Jane Tran <ttran161@ucsc.edu>
2023-11-07 16:01:40 -0800

    D3.a complete

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 
-
 const MERRILL_CLASSROOM = leaflet.latLng({
-    lat: 36.9995,
-    lng: - 122.0533
+  lat: 36.9995,
+  lng: -122.0533,
 });
 
+let collectedCoins = 0;
+const deposits: { [key: string]: number } = {};
 const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
@@ -18,18 +19,21 @@ const PIT_SPAWN_PROBABILITY = 0.1;
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
-    center: MERRILL_CLASSROOM,
-    zoom: GAMEPLAY_ZOOM_LEVEL,
-    minZoom: GAMEPLAY_ZOOM_LEVEL,
-    maxZoom: GAMEPLAY_ZOOM_LEVEL,
-    zoomControl: false,
-    scrollWheelZoom: false
+  center: MERRILL_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
 
-leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
-}).addTo(map);
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
 
 const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
@@ -37,10 +41,12 @@ playerMarker.addTo(map);
 
 const sensorButton = document.querySelector("#sensor")!;
 sensorButton.addEventListener("click", () => {
-    navigator.geolocation.watchPosition((position) => {
-        playerMarker.setLatLng(leaflet.latLng(position.coords.latitude, position.coords.longitude));
-        map.setView(playerMarker.getLatLng());
-    });
+  navigator.geolocation.watchPosition((position) => {
+    playerMarker.setLatLng(
+      leaflet.latLng(position.coords.latitude, position.coords.longitude)
+    );
+    map.setView(playerMarker.getLatLng());
+  });
 });
 
 let points = 0;
@@ -48,39 +54,78 @@ const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
-    const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
-    ]);
+  const bounds = leaflet.latLngBounds([
+    [
+      MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+      MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+    ],
+    [
+      MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
+      MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
+    ],
+  ]);
 
-    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+  const pit = leaflet.rectangle(bounds) as leaflet.Layer;
 
+  pit.bindPopup(() => {
+    let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+    let coins = Math.floor(luck([i, j, "coinValue"].toString()) * 10);
+    if (!deposits[`${i},${j}`]) {
+      deposits[`${i},${j}`] = 0;
+    }
+    const container = document.createElement("div");
+    container.innerHTML = `
+        <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>. It has <span id="coins"> ${coins}</span> coins.</div>
+        <button id="poke">poke</button>
+        <button id="collectCoins">Collect</button>
+        <button id="depositCoins">Deposit</button>
+        <div>Collecting: <span id="inventory">${collectedCoins}</span></div>
+        <div>Depositing: <span id="deposit">${
+          deposits[`${i},${j}`]
+        }</span></div>`;
 
+    const poke = container.querySelector<HTMLButtonElement>("#poke")!;
+    const inventoryDisplay =
+      container.querySelector<HTMLSpanElement>("#inventory")!;
+    const collectButton =
+      container.querySelector<HTMLButtonElement>("#collectCoins")!;
+    const depositButton =
+      container.querySelector<HTMLButtonElement>("#depositCoins")!;
 
-    pit.bindPopup(() => {
-        let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
-        const container = document.createElement("div");
-        container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="poke">poke</button>`;
-        const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-        poke.addEventListener("click", () => {
-            value--;
-            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
-            points++;
-            statusPanel.innerHTML = `${points} points accumulated`;
-        });
-        return container;
+    poke.addEventListener("click", () => {
+      if (value > 0) {
+        value--;
+        container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+          value.toString();
+        points++;
+        statusPanel.innerHTML = `${points} points accumulated`;
+      }
     });
-    pit.addTo(map);
+    collectButton.addEventListener("click", () => {
+      collectedCoins += coins;
+      coins = 0;
+      container.querySelector<HTMLDivElement>("#coins")!.innerHTML =
+        coins.toString();
+      inventoryDisplay.innerHTML = collectedCoins.toString();
+    });
+    depositButton.addEventListener("click", () => {
+      if (collectedCoins > 0) {
+        deposits[`${i},${j}`] += collectedCoins;
+        collectedCoins = 0;
+        inventoryDisplay.textContent = collectedCoins.toString();
+        container.querySelector<HTMLSpanElement>("#deposit")!.innerHTML =
+          deposits[`${i},${j}`].toString();
+      }
+    });
+    return container;
+  });
+  pit.addTo(map);
 }
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-    for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-        if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-            makePit(i, j);
-        }
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+      makePit(i, j);
     }
+  }
 }
